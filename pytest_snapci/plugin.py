13dddf1c456d9158516741c2f248977cd96787b5
Omer Katz <omer.drow@gmail.com>
2015-11-12 21:31:40 +0200

    Adjusted method and variables names.

--- pytest_snapci/plugin.py
+++ pytest_snapci/plugin.py
@@ -6,21 +6,21 @@ class SnapCIError(Exception):
     """Raised for problems running the Snap-CI py.test plugin"""
 
 
-def read_circleci_env_variables():
+def read_snapci_env_variables():
     """Read and convert SNAP_* environment variables"""
-    snap_node_total = int(os.environ.get("SNAP_WORKER_TOTAL", "1").strip())
-    snap_node_index = int(os.environ.get("SNAP_WORKER_INDEX", "1").strip())
+    snap_worker_total = int(os.environ.get("SNAP_WORKER_TOTAL", "1").strip())
+    snap_worker_index = int(os.environ.get("SNAP_WORKER_INDEX", "1").strip())
 
-    if snap_node_index > snap_node_total:
-        raise SnapCIError("SNAP_WORKER_INDEX={} >= SNAP_WORKER_TOTAL={}, should be less".format(snap_node_index, snap_node_total))
+    if snap_worker_index > snap_worker_total:
+        raise SnapCIError("SNAP_WORKER_INDEX={} >= SNAP_WORKER_TOTAL={}, should be less".format(snap_worker_index, snap_worker_total))
 
-    return (snap_node_total, snap_node_index)
+    return (snap_worker_total, snap_worker_index)
 
 
 def pytest_report_header(config):
     """Add Snap-CI information to report"""
-    snap_node_total, snap_node_index = read_circleci_env_variables()
-    return "Snap-CI total nodes: {}, this node index: {}".format(snap_node_total, snap_node_index)
+    snap_worker_total, snap_worker_index = read_snapci_env_variables()
+    return "Snap-CI total nodes: {}, this node index: {}".format(snap_worker_total, snap_worker_index)
 
 
 def pytest_collection_modifyitems(session, config, items):
@@ -33,11 +33,11 @@ def pytest_collection_modifyitems(session, config, items):
     Will run a subset of tests based on the node index.
 
     """
-    snap_node_total, snap_node_index = read_circleci_env_variables()
+    snap_worker_total, snap_worker_index = read_snapci_env_variables()
     deselected = []
     for index, item in enumerate(list(items)):
         item_hash = int(hashlib.sha1(u':'.join(map(str, item.location)).encode('utf-8')).hexdigest(), 16)
-        if (item_hash % snap_node_total) != snap_node_index:
+        if (item_hash % snap_worker_total) != snap_worker_index:
             deselected.append(item)
             items.remove(item)
     config.hook.pytest_deselected(items=deselected)
